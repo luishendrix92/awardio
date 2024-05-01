48d6f3474001d6e45bbd383461648f3841f9f9be
Luis Lopez <luishendrix92@gmail.com>
2024-05-01 10:25:09 -0700

    Create repository interfaces

---
+++ api/src/main/java/com/kozmicblog/awardio/repository/AwardRepository.java
@@ -0,0 +1,9 @@
+package com.kozmicblog.awardio.repository;
+
+import com.kozmicblog.awardio.model.Award;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AwardRepository extends CrudRepository<Award, Integer> {
+}
---
+++ api/src/main/java/com/kozmicblog/awardio/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kozmicblog.awardio.repository;
+
+import com.kozmicblog.awardio.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
