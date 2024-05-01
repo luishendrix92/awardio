f5342765e5e73324f2bd8db9155b6ef9d40f89da
Luis Lopez <luishendrix92@gmail.com>
2024-05-01 12:09:35 -0700

    Remove unused imports

--- api/src/main/java/com/kozmicblog/awardio/controller/ShowController.java
+++ api/src/main/java/com/kozmicblog/awardio/controller/ShowController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/shows")
